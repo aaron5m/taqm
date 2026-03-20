bb1f80b9d29e2d827b2da6eb2e44a4c3e13fb7f1
Aaron Mitchell <me@paaronmitchell.com>
2026-03-20 22:08:45 +0700

    update vite.config.js with opened ports and hostname for server hot development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+/*
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,3 +12,24 @@ export default defineConfig({
     'process.env.VITE_PASS_URL': JSON.stringify(process.env.VITE_PASS_URL)
   }
 })
+*/
+
+const vitePassUrl = JSON.stringify(process.env.VITE_PASS_URL);
+
+let config = {
+  plugins: [react()],
+  build: { outDir: "/app/dist" },
+  define: {
+    'process.env.VITE_PASS_URL': vitePassUrl
+  }
+};
+
+if (vitePassUrl) {
+  config.server = {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [new URL(vitePassUrl).hostname]
+  };
+}
+
+export default defineConfig(config);
